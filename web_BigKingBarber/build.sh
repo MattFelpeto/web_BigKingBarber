7aa5127a1f49d0235c53e50e8ce5c4985efa858c
MattEzequiel <matias.felpeto@gmail.com>
2025-11-03 21:29:58 -0300

    cambios en yaml

--- web_BigKingBarber/build.sh
+++ web_BigKingBarber/build.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 set -o errexit
 
+# 1. Instalar dependencias
 pip install -r requirements.txt
 
+# 2. Recopilar archivos estáticos (¡Necesario durante el build!)
 python manage.py collectstatic --no-input
 
-python manage.py migrate
-
-ls -R
+# 3. NOTA: El comando 'migrate' se ha movido al startCommand en render.yaml
